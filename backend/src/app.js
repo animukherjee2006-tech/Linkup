ebdf4e57ced59dd4aa3c7f06e95a6c645c15797f
Ani Mukherjee <animukherjee2006@gmail.com>
2026-05-03 12:12:54 +0530

    message

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app= express();
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
-    origin: 'http://localhost:5173', // Frontend URL
+    origin: 'https://linkup-1-frontend.onrender.com', // Frontend URL
     credentials: true,               
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
